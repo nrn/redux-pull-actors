503fb3de267c8dfc4f88b2206ce4f58406ce5700
Nick Niemeir <nick.niemeir@gmail.com>
2016-07-13 18:52:34 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var pull = require('pull-stream')
+var notify = require('pull-notify')
+var reduce = require('universal-reduce')
+
+module.exports = actorMiddleware
+
+function actorMiddleware (actors) {
+  return function createMiddleware (store) {
+    var publish = notify()
+    reduce(actors, function (_, stream, n) {
+      var source = pull(publish.listen(), stream(store))
+      if (source) {
+        console.log('here ', n)
+        pull(source, pull.drain(function (action) {
+          if (action != null) store.dispatch(action)
+        }))
+      }
+    })
+    return function (next) {
+      return function (action) {
+        var before = store.getState()
+        var res = next(action)
+        var state = store.getState()
+        publish({ action, before, state })
+        return res
+      }
+    }
+  }
+}
